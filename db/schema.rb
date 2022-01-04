eaecf882b476c6e5ebacc30b37a46a7ef2668dcd
N.Ooe <natsuo.tatble@gmail.com>
2022-01-04 20:35:29 +0900

    userとmicropostモデル生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_103740) do
+ActiveRecord::Schema.define(version: 2022_01_04_105002) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
