cbbf300de4e30404a18ce074c76dcb50380e5c1a
N.Ooe <natsuo.tatble@gmail.com>
2022-05-08 20:00:05 +0900

    マクロポストDB削除

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2022_05_05_122029) do
 
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
