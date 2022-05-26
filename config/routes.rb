58048ca200c69dea5f173ee61c8bd1b22cfabff1
N.Ooe <natsuo.tatble@gmail.com>
2022-05-26 21:40:40 +0900

    user周り

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
+# Rails.application.routes.draw do
+#   get 'users/new'　これがどんな影響を生むかわからん
+#   root 'static_pages#home'
+#   get  '/help',    to: 'static_pages#help'
+#   get  '/about',   to: 'static_pages#about'
+#   get  '/contact', to: 'static_pages#contact'
+#   get  '/signup',  to: 'users#new'
+# end　
+
+
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  resources :users
 end
