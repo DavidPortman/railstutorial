2c9ef06c19c5106c02e0891957e82bf6b0a9c622
N.Ooe <natsuo.tatble@gmail.com>
2022-06-07 21:59:57 +0900

    エラー文テスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+
 # Rails.application.routes.draw do
 #   get 'users/new'　これがどんな影響を生むかわからん
 #   root 'static_pages#home'
@@ -14,5 +15,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  resources :users
+  resources :users #resourcesメソッド index/show/new/create/edit/update/destroyを含む
 end
