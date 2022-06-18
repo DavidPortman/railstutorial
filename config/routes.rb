f29fcf88beff6786c29020c9e2e113104c391363
N.Ooe <natsuo.tatble@gmail.com>
2022-06-18 21:36:48 +0900

    sessionを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 
 # Rails.application.routes.draw do
-  get 'sessions/new'
+  
 #   get 'users/new'　これがどんな影響を生むかわからん
 #   root 'static_pages#home'
 #   get  '/help',    to: 'static_pages#help'
@@ -11,11 +11,14 @@
 
 
 Rails.application.routes.draw do
-  get 'sessions/new'
+  
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users #resourcesメソッド index/show/new/create/edit/update/destroyを含む
 end
