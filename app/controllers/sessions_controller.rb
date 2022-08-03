ac58a5b5f36a6a9b0e2efba6d3d3c15eff1b7a1d
N.Ooe <natsuo.tatble@gmail.com>
2022-08-03 21:57:50 +0900

    assignsメソッド

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user #実際はuser_url(user)のこと
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user #実際はuser_url(user)のこと
     else
       # エラーメッセージを作成する
       #flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
