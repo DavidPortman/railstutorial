c798afe8d931567eb02df060a7b466c21da925f9
N.Ooe <natsuo.tatble@gmail.com>
2022-09-24 21:55:48 +0900

    ふれフォワ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,7 @@ class UsersController < ApplicationController
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
@@ -58,5 +59,6 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
+
     
 end
